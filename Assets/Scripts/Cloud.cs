1376f35db42ff4eb511a062be71925983ee299d6
Tyler S-C <tscizzle@gmail.com>
2019-09-15 01:00:44 -0400

    kinda have a cloud around the player, but it doesnt go away based on altitude

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    private static float cloudsAltitude = 200;
+
+    private MeshRenderer cloudRenderer;
+
+    void Start()
+    {
+        cloudRenderer = GetComponent<MeshRenderer>();
+        cloudRenderer.enabled = false;
+    }
+
+    void Update()
+    {
+        if (transform.position.y <= cloudsAltitude)
+        {
+            cloudRenderer.enabled = true;
+        } else
+        {
+            cloudRenderer.enabled = false;
+        }
+    }
+}
